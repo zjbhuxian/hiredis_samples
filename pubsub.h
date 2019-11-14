e038f4aad0d4b5f1316af6a942eff226cdbdc096
zjbhuxian <zjbhuxian@gmail.com>
2019-11-14 20:43:27 +0800

    update

--- pubsub.h
+++
@@ -1,9 +0,0 @@
-#include <signal.h>
-#include <hiredis/hiredis.h>
-#include <hiredis/async.h>
-#include <hiredis/adapters/libevent.h>
-
-void subCallback(redisAsyncContext* rc, void* r, void* priv);
-void connectCallback(const redisAsyncContext* rc, int status);
-void disconnectCallback(const redisAsyncContext* rc, int status);
-
